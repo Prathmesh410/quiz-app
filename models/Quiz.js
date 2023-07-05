08735693f221581a5e7c6fbf6eeb153dc748fb6d
Prathemsh <prathmeshsonawane09538@gmail.com>
2023-07-05 17:58:31 +0530

    Quiz and question schema

---
+++ models/Quiz.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const quizSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  questions: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Question'
+  }],
+  participants: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Participant'
+  }]
+});
+
+module.exports = mongoose.model('Quiz', quizSchema);
