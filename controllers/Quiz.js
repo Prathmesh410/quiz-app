e8bfd6e9c656834cbe8547d0384f03be6fa37ce3
Prathemsh <prathmeshsonawane09538@gmail.com>
2023-07-06 18:09:39 +0530

    Quiz routes added

--- controllers/Quiz.js
+++ controllers/Quiz.js
@@ -44,8 +44,94 @@ const getQuizById = async (req, res) => {
     }
   };
 
+  const updateQuiz = async (req, res) => {
+    try {
+      const quizId = req.params.id;
+      const { title } = req.body;
+  
+      // Find and update the quiz
+      const updatedQuiz = await Quiz.findByIdAndUpdate(
+        quizId,
+        { title },
+        { new: true }
+      );
+  
+      if (!updatedQuiz) {
+        return res.status(404).json({ error: 'Quiz not found' });
+      }
+  
+      res.json(updatedQuiz);
+    } catch (error) {
+      res.status(500).json({ error: 'Failed to update the quiz' });
+    }
+  };
+
+  const deleteQuiz = async (req, res) => {
+    try {
+      const quizId = req.params.id;
+      
+      // Find and delete the quiz
+      const deletedQuiz = await Quiz.findByIdAndDelete(quizId);
+  
+      if (!deletedQuiz) {
+        return res.status(404).json({ error: 'Quiz not found' });
+      }
+  
+      res.json({ message: 'Quiz deleted successfully' });
+    } catch (error) {
+      res.status(500).json({ error: 'Failed to delete the quiz' });
+    }
+  };
+
+  const shareQuiz = async (req, res) => {
+    try {
+      const quizId = req.params.id;
+    
+      const shareableLink = `http://localhost:8000/api/quizzes/${quizId}`;
+  
+      res.json({ shareableLink });
+    } catch (error) {
+      res.status(500).json({ error: 'Failed to generate the shareable link' });
+    }
+  };
+
+  const getSharedQuiz = async (req, res) => {
+    try {
+      const shareableLink = req.params.link;
+  
+      // Find the quiz by the shared link
+      const quiz = await Quiz.findOne({ shareableLink });
+  
+      if (!quiz) {
+        return res.status(404).json({ error: 'Quiz not found' });
+      }
+  
+      res.json(quiz);
+    } catch (error) {
+      res.status(500).json({ error: 'Failed to retrieve the quiz' });
+    }
+  };
+
+  const getParticipantScores = async (req, res) => {
+    try {
+      const participantId = req.params.participantId;
+  
+      // Find the scores of the participant for all quizzes
+      const scores = await Score.find({ participantId });
+  
+      res.json(scores);
+    } catch (error) {
+      res.status(500).json({ error: 'Failed to retrieve the scores' });
+    }
+  };
+
 module.exports = {
   createQuiz,
   getQuizById,
-  getAllQuizzes
+  getAllQuizzes,
+  updateQuiz,
+  deleteQuiz,
+  shareQuiz,
+  getSharedQuiz,
+  getParticipantScores
 };
--- routes/Quiz.js
+++ routes/Quiz.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const {createQuiz,getQuizById,getAllQuizzes} = require('../controllers/Quiz');
+const {createQuiz,getQuizById,getAllQuizzes,updateQuiz,deleteQuiz,shareQuiz,getParticipantScores} = require('../controllers/Quiz');
 
 router.post('/quizzes',createQuiz);
 router.get('/quizzes/:id', getQuizById);
 router.get('/quizzes', getAllQuizzes);
+router.put('/quizzes/:id', updateQuiz);
+router.delete('/quizzes/:id', deleteQuiz);
+
+
+
+//to this leter
+router.get('/quizzes/:id/share', shareQuiz);
+router.get('/participants/:participantId/scores',getParticipantScores);
 
 module.exports = router;
