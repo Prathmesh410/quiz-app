84698632f4e9981913d0455df57e14adb171b471
Prathemsh <prathmeshsonawane09538@gmail.com>
2023-07-08 09:38:03 +0530

    userid middleware is added

--- controllers/auth.js
+++ controllers/auth.js
@@ -77,3 +77,20 @@ exports.isAuthenticated = (req,res,next) => {
   }
   next()
 };
+
+ exports.getUserByIdMiddleware = async (req, res, next, id) => {
+  try {
+    const user = await User.findById(id);
+
+    if (!user) {
+      return res.status(400).json({
+        error: 'User not found',
+      });
+    }
+
+    req.profile = user;
+    next();
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to retrieve the user' });
+  }
+};
